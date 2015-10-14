2a2cedc7e21bf2f81f35e167d806743ad760f361
Daniel Navon <dnavon2557@gmail.com>
2015-10-13 21:22:29 -0400

    Use Javascript and canvas to create webpage that display the duckhunt background with two birds on it

---
+++ duckhunt/README.txt
@@ -0,0 +1,11 @@
+					README
+AUTHOR: Daniel Navon
+LAST MODIFIED: 10/13/2015
+DESCRIPTION: A webpage that uses the <canvas> tag and javascript to display a 
+duckhunt background with two birds
+IMPLEMENTATION: The javascript uses the context from the canvas tag to draw the 
+background and birds using drawImage. The script also waits for the images to 
+finish loading before drawing them to the canvas using .EventListener. The 
+images are cut out from a sprite sheet.
+TIME SPENT: 1 hour to make the current file. Spent 3 hours trying to animate 
+but gave up.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,18 @@
-// Replace this with your code...
+function init() {
+	canvas = document.getElementById('game_canvas');	
+	ctx = canvas.getContext('2d');
+	background = new Image();
+	background.src = 'duckhunt-background.gif';	
+	//wait until image loads to drawImage
+	background.addEventListener("load", function() {
+		ctx.drawImage(background, 0, 0);
+	}, false);
+	sprites = new Image();
+	sprites.src = 'duckhunt_various_sheet.png';
+		//wait until image loads to drawImage
+	sprites.addEventListener("load", function() {
+		ctx.drawImage(sprites, 0, 120, 34, 24, 175, 45, 34, 24);
+		ctx.drawImage(sprites, 130, 120, 34, 24, 75, 80, 34, 24);
+	}, false);
+
+}
