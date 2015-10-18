95794f1af86924c011b028e455e8014ac82801e1
Daniel Navon <dnavon2557@gmail.com>
2015-10-18 18:40:03 -0400

    Added script to load data from data.json

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,12 @@
 // Your JavaScript goes here...
+function parse() {
+	data = new XMLHttpRequest();
+	data.open("GET", "data.json", true);
+	data.onreadystatechange = function () {
+		if ( data.readyState == 4) {
+			messages = document.getTagByID("messages");
+			messages.innerHTML = data.responseText;
+
+		}
+	}
+}
